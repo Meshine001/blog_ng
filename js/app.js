508d497e619a918132ccf13279e4f249b0276d4c
Meshine001 <mingshuai001@hotmail.com>
2017-06-28 14:29:15 +0800

    进行了主页模块划分，添加http数据

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Ming on 2017/6/27.
  */
-var routerApp = angular.module('routerApp', ['ui.router', 'ngGrid']);
+var routerApp = angular.module('routerApp', ['ui.router', 'ArticleListMoudle']);
 /**
  * 由于整个应用都会和路由打交道，所以这里把$state和$stateParams这两个对象放到$rootScope上，方便其它地方引用和注入。
  * 这里的run方法只会在angular启动的时候运行一次。
@@ -31,6 +31,12 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
             views:{
                 '':{
                     templateUrl:'views/home.html'
+                },
+                'articleList@index':{
+                    templateUrl:'views/articleList.html'
+                },
+                'homeAside@index':{
+                    templateUrl:'views/homeAside.html'
                 }
             }
         })
--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,12 @@
 /**
  * Created by Ming on 2017/6/27.
  */
+
+var articleListMoudle = angular.module('ArticleListMoudle',[]);
+articleListMoudle.controller('ArticleListCtrl',function ($scope,$http) {
+    $http
+        .get('data/articleList.json')
+        .success(function (data) {
+            $scope.articles = data;
+        });
+});
